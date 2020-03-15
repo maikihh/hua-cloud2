6eda1b7d95fbbdd1698360d139c955533909c293
maikihh@163.com <maikihh@163.com>
2020-03-15 15:21:46 +0800

    delete undesired AppDemo

--- AppDemo/src/test/java/framework/PageObjectModel.java
+++
@@ -1,9 +0,0 @@
-package framework;
-
-import java.util.HashMap;
-
-public class PageObjectModel {
-    public HashMap<String, PageObjectElement> elements = new HashMap<>();
-    public HashMap<String, PageObjectMethod> methods = new HashMap<>();
-
-}
